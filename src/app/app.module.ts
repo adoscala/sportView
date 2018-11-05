0211bd8e3c47e43808a92d8dfab129daaad9f97b
Bruno Camargo <brunocam1195@gmail.com>
2018-11-05 17:44:26 -0300

    Obligado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistrateComponent } from './components/registrate/registrate.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrateComponent
+    RegistrateComponent,
+    InicioComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
