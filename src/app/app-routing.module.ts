84406e9b794dbf3dac5c6870ec80577311d27abd
Bruno Camargo <brunocam1195@gmail.com>
2018-11-13 12:03:29 -0300

    Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   { path:'', component: InicioComponent},
-  { path:'registrate', component: RegistrateComponent},
+  { path:'registar', component: RegistrateComponent},
   { path:'login', component: LoginComponent}
 ];
 
